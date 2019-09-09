99b1593650f6fb468fba200accf605dfb99dbf28
wandersonjackson <wanderson@bitofproperty.com>
2019-09-10 00:43:11 +0300

    list items

--- wandersonjackson/src/components/navbar/navbar.js
+++ wandersonjackson/src/components/navbar/navbar.js
@@ -4,7 +4,22 @@ export default class Navbar extends Component {
   render() {
     return (
       <div>
-        hello
+        <header>
+            <div className="menu">
+                <div>
+                    <a href="#">
+                        Hello
+                    </a>
+                </div>
+                <nav>
+                    <ul>
+                        <li> <a>Hello</a></li>
+                        <li> <a>Hello</a></li>
+                        <li> <a>Hello</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
       </div>
     )
   }
