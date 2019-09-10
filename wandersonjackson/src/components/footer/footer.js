777bad0a41f33db300f9cc7929f32864b0c2d66f
wandersonjackson <wanderson@bitofproperty.com>
2019-09-10 23:34:36 +0300

    css fraction and addition into project from deploy to local

---
+++ wandersonjackson/src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+
+export default class Footer extends Component {
+  render() {
+    return
+     <div>
+         
+    </div>
+  }
+}
